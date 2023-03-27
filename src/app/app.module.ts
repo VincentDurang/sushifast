ff2eff93692786ac2b83574142c26997f81f31bd
Anthony <anthonydantaspaiva@gmail.com>
2023-03-27 14:06:14 +0200

    detail+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,7 @@ import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './component/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { A11yModule } from '@angular/cdk/a11y';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -27,15 +25,9 @@ import { FormsModule } from '@angular/forms';
     NgbModule,
     HttpClientModule,
     ReactiveFormsModule,
-    CommonModule,
-    A11yModule,
-    FormsModule
-  ],
-  exports: [
-    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class HomeModule { }
+
