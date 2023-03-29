dc0b06df6e7382652c9541758377a820441da2af
theop <theo.pinceel@gmail.com>
2023-03-29 15:35:29 +0200

    estheticienne

--- src/app/component/panier/panier.component.ts
+++ src/app/component/panier/panier.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Box } from 'src/app/models/Box';
+import { IBoxs } from 'src/app/models/iBoxes';
+import { ManagerBoxService } from 'src/app/service/manager-box.service';
 
 @Component({
   selector: 'app-panier',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class PanierComponent {
 
+  
+  constructor(private managerBoxService: ManagerBoxService) {
+  
+  }
+
+
+  ngOnInit(): void {
+    this.managerBoxService.editHackerEvent
+      .subscribe((box: IBoxs) => { 
+        console.log('Event message editEvent')
+      
+        
+      })
+  }
 }
--- src/app/service/manager-box.service.ts
+++ src/app/service/manager-box.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable, Output } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Box } from '../models/Box';
 import { IBoxs } from '../models/iBoxes';
@@ -17,8 +17,22 @@ export class ManagerBoxService {
   constructor(private http:HttpClient) {
 
   }
+
+  @Output() editHackerEvent = new EventEmitter<IBoxs>()
+  @Output() updateHackersListEvent = new EventEmitter<IBoxs[]>()
+  
   getAllBoxes() : Observable<any> {
    return this.http.get(environment.apiBaseUrl+'api/boxes')
+  }
+
+  getPanier(): Box[] {
+    return JSON.parse(localStorage.getItem('box') || '[]');
+  }
+
+  setBoxes(box: Box) {
+    let boxes = this.getAllBoxes();
+    localStorage.setItem('box', JSON.stringify(boxes));
+    this.updateHackersListEvent.emit(this.boxes)
   }
 }
 
